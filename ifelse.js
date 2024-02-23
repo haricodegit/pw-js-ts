b44a8c1f602bf2cbc7e897ba62afa860aa595b32
VandanaEdiga <vandanagoud28@gmail.com>
2024-02-23 21:39:20 +0530

    message

---
+++ ifelse.js
@@ -0,0 +1,8 @@
+function evenodd(num){
+    if(num%2==0)
+    console.log("Even number");
+    else
+    console.log("Odd number")
+}
+evenodd(567);
+evenodd(5);
---
+++ variable.js
@@ -0,0 +1,7 @@
+var a=9;
+function test(){
+  var b=5;
+}
+console.log(a);
+const x=10
+console.log(x)
